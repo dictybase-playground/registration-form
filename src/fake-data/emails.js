3f487999fd49076db10f884e488c915607c98546
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-15 14:20:28 -0500

    add fake email data

---
+++ src/fake-data/emails.js
@@ -0,0 +1,18 @@
+const emails = [
+    {
+        "test@test.com": {
+            google: "test@gmail.com",
+            orcid: 999999999
+        },
+        "fake@fakeaccount.com": {
+            orcid: 888888888,
+            facebook: "test@outlook.com"
+        },
+        "allemails@allemails.com": {
+            google: "all@gmail.com",
+            orcid: 777777777,
+            facebook: "all@outlook.com",
+            linkedin: "all@gmail.com"
+        }
+    }
+]
