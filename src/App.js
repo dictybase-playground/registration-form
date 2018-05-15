a67b4e02fe32d3019f02426163a8156b594572d9
wildlifehexagon <crossthebreeze@gmail.com>
2018-05-15 16:27:48 -0500

    refactor new user form to match user api structure

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import Grid from "material-ui/Grid"
 import { Background } from "./styles"
 
 const App = () => {
-    return (
-        <Background>
-            <center>
-                <h1>dictyBase Registration Form</h1>
-            </center>
-            <Grid container justify="center" spacing={24}>
-                <Grid item xs={7}>
-                    <Paper style={{ padding: "10px" }}>
-                        <MainEmailForm />
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Background>
-    )
+  return (
+    <Background>
+      <center>
+        <h1>dictyBase Registration Form</h1>
+      </center>
+      <Grid container justify="center" spacing={24}>
+        <Grid item xs={7}>
+          <Paper style={{ padding: "10px" }}>
+            <MainEmailForm />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Background>
+  )
 }
 
 export default App
